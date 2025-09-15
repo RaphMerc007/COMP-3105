26dc2df1a2058979a015e624e1f0e9a7b64067c8
Raphael Mercier <raphaelmercier@softguard007.com>
2025-09-15 16:17:01 -0400

    Adding base for A1

---
+++ A1files/Implementation/A1codes.py
@@ -0,0 +1,31 @@
+# COMP 3105 Fall 2025 Assignment 1
+# Raphael Mercier & Patrick
+# Machine Learning Optimization Algorithms Implementation
+
+import numpy as np
+from scipy.optimize import minimize
+
+
+def minimizeL2(X, y):
+    # TODO: Implement L2 regression
+    pass
+
+
+def minimizeLinf(X, y):
+    # TODO: Implement L∞ regression
+    pass
+
+
+def logisticRegObj(w, X, y):
+    # TODO: Implement logistic regression objective
+    pass
+
+
+def logisticRegGrad(w, X, y):
+    # TODO: Implement logistic regression gradient
+    pass
+
+
+def find_opt(obj_func, grad_func, X, y):
+    # TODO: Implement optimization routine
+    pass
---
+++ A1files/Implementation/__init__.py
@@ -0,0 +1,7 @@
+# RaphaelMercier - COMP 3105 Fall 2025 Assignment 1
+# Group Project: Raphael Mercier & Patrick
+# Machine Learning Optimization Algorithms
+
+__version__ = "1.0.0"
+__author__ = "Raphael Mercier"
+__group_members__ = ["Raphael Mercier", "Patrick"]
