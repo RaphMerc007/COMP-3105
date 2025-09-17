b6d126036ad8c158d3ab1d6e0bb5871e8a0c0a67
Raphael Mercier <raphaelmercier@softguard007.com>
2025-09-17 15:51:14 -0400

    Completing 1b

--- A1files/Implementation/A1codes.py
+++ A1files/Implementation/A1codes.py
@@ -2,6 +2,7 @@
 # Raphael Mercier & Patrick
 # Machine Learning Optimization Algorithms Implementation
 
+from cvxopt import matrix, solvers
 import numpy as np
 from scipy.optimize import minimize
 
@@ -13,8 +14,40 @@ def minimizeL2(X, y):
 
 
 def minimizeLinf(X, y):
-  # TODO: Implement L∞ regression
-  pass
+  # Define shape of matrix
+  n = X.shape[0]
+  d = X.shape[1]
+  
+  # c^⊤ u = δ
+  c = np.zeros[1, d+1]
+  c[d] = [1]
+
+  # G(1)u ⪯ h(1) ⇐⇒ δ ≥ 0
+  h1 = [0]
+  G1 = np.zeros[d+1, 1]
+  G1[d] = -1
+
+  # G(2)u ⪯ h(2) ⇐⇒ Xw − y ⪯ δ·1n
+  h2 = y
+  G2 = np.concatenate([X, -1 * np.ones(n, 1)], axis=1)
+
+  # G(3)u ⪯ h(3) ⇐⇒ y − Xw ⪯ δ·1n
+  h3 = -y
+  G3 = np.concatenate([-X, -1 * np.ones(n, 1)], axis=1)
+
+  # Combine parts of G and h together
+  G = np.concatenate([G1, G2, G3], axis=0)
+  h = np.concatenate([h1, h2, h3], axis=0)
+
+  # convert to cvxopt matrix
+  c = matrix(c)
+  G = matrix(G)
+  h = matrix(h)
+
+  # TODO: Need to uncomment this before submition
+  # solvers.options['show progress'] = False
+
+  return solvers.lp(c,G,h)
 
 
 def logisticRegObj(w, X, y):
