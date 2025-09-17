fd153f539be81488e332f49014793ad061ebe446
Raphael Mercier <raphaelmercier@softguard007.com>
2025-09-17 15:17:14 -0400

    Adding comments

--- A1files/Implementation/A1codes.py
+++ A1files/Implementation/A1codes.py
@@ -7,24 +7,26 @@ from scipy.optimize import minimize
 
 
 def minimizeL2(X, y):
-    return np.linalg.inv(X.T @ X) @ X.T @ y
+  # This follows the equation:
+  # (X^T * X)^−1 * X^T * y
+  return np.linalg.inv(X.T @ X) @ X.T @ y
 
 
 def minimizeLinf(X, y):
-    # TODO: Implement L∞ regression
-    pass
+  # TODO: Implement L∞ regression
+  pass
 
 
 def logisticRegObj(w, X, y):
-    # TODO: Implement logistic regression objective
-    pass
+  # TODO: Implement logistic regression objective
+  pass
 
 
 def logisticRegGrad(w, X, y):
-    # TODO: Implement logistic regression gradient
-    pass
+  # TODO: Implement logistic regression gradient
+  pass
 
 
 def find_opt(obj_func, grad_func, X, y):
-    # TODO: Implement optimization routine
-    pass
+  # TODO: Implement optimization routine
+  pass
