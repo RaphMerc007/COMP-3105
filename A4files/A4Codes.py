4a88175ab61d2ebbc624e42d51a0e00f7d12f424
Raphael Mercier <raphaelmercier@softguard007.com>
2025-11-17 14:07:30 -0500

    Converting images properly

--- A4files/A4Codes.py
+++ A4files/A4Codes.py
@@ -1,12 +1,12 @@
 # TODO: Add necessary imports
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torch.utils.data import Dataset, DataLoader
-# import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
 import os
 from PIL import Image
-# from pathlib import Path
+from pathlib import Path
 
 
 in_train = []
@@ -15,6 +15,14 @@ in_test = []
 out_test = []
 class_to_idx = {"Alarm_Clock": 0, "Bike": 1, "Desk_Lamp": 2, "Monitor": 3, "Mouse": 4, 
                 "Oven": 5, "Pencil": 6, "Radio": 7, "Refrigerator": 8, "Screwdriver": 9}
+
+def get_transforms():
+  transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Make all images same size
+    transforms.ToTensor(),  # Convert PIL to tensor
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
+  ])
+  return transform
 
 def load_data(path_to_in_domain, path_to_out_domain):
   in_train.clear()
@@ -26,7 +34,8 @@ def load_data(path_to_in_domain, path_to_out_domain):
       for file in os.listdir(folder_path):
         if file.endswith('.jpg'):
           image = Image.open(os.path.join(folder_path, file)).convert('RGB')
-          label = class_to_idx[folder]  
+          label = class_to_idx[folder] 
+          image = get_transforms()(image)
           in_train.append((image, label))
   
   unlabelled_path = os.path.join(path_to_out_domain, 'unlabelled')
@@ -34,12 +43,15 @@ def load_data(path_to_in_domain, path_to_out_domain):
     for file in os.listdir(unlabelled_path):
       if file.endswith('.jpg'):
         image = Image.open(os.path.join(unlabelled_path, file)).convert('RGB')
+        image = get_transforms()(image)
         out_train.append(image)
 
 
 def learn(path_to_in_domain, path_to_out_domain):
-  # TODO: Implement training logic
-  # - Load data
+  load_data(path_to_in_domain, path_to_out_domain)
+  
+  
+
   # - Initialize model
   # - Train (consider using out-domain data to minimize out-domain accuracy)
   # - Return trained model
@@ -55,4 +67,4 @@ def compute_accuracy(path_to_eval_folder, model):
   pass
 
 
-load_data('./A4data/in-domain-train', './A4data/out-domain-train')
+learn('./A4data/in-domain-train', './A4data/out-domain-train')
